d4e32a94060ddefad4226aa797f17c7faaf26f9e
Slava Adnavorau <slava.adnavorau@gmail.com>
2019-12-10 13:05:37 -0500

    Final

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -9,17 +9,26 @@
 // })
 
 describe('Bubble Sort', () => {
-  // it('number of swap calls', () => {
-  //   bubbleSort([]);
-  //   expect(window.swap.calls.count()).toEqual(1);
-  // })
+  beforeEach(() => {
+    spyOn(window, 'swap').and.callThrough();
+    spyOn(window, 'compare').and.callThrough();
+  });
+  it('number of swap calls', () => {
+    bubbleSort([1, 2, 3, 5, 4]);
+    expect(swap.calls.count()).toEqual(1);
+  });
+  it('number of compare calls', () => {
+    bubbleSort([1, 2, 3, 5, 4]);
+    expect(compare.calls.count()).toEqual(8);
+  });
+
   it('handles an empty array', () => {
-    expect( bubbleSort([]) ).toEqual([]);
-  })
+    expect(bubbleSort([])).toEqual([]);
+  });
   it('sorts properly', () => {
-    expect( bubbleSort([7,1,2,3,4,5,6]) ).toEqual([1,2,3,4,5,6,7]);
-    expect( bubbleSort([5,4,3,2,1]) ).toEqual([1,2,3,4,5]);
-    expect( bubbleSort([3,4,2,1,5]) ).toEqual([1,2,3,4,5]);
-    expect( bubbleSort([1,2,3,4,5]) ).toEqual([1,2,3,4,5]);
-  })
-})
+    expect(bubbleSort([7, 1, 2, 3, 4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    expect(bubbleSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+    expect(bubbleSort([3, 4, 2, 1, 5])).toEqual([1, 2, 3, 4, 5]);
+    expect(bubbleSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+});
